22e911bf72c400ba21ab376792bf2adf7c4a0caf
Silvia <sramos.exp@gmail.com>
2020-09-29 17:35:34 +0100

    event listener added

--- js/app.js
+++ js/app.js
@@ -11,12 +11,20 @@ const activateClass = clicked => {
   panels.forEach(panel => {
     if (panel === clicked) {
       panel.classList.add('active');
-      h3.classList.add('rotateActive');
-      section.style.display = 'block';
+      h3.forEach(h3 => { 
+        h3.classList.add('rotateActive');
+        });
+      section.forEach(section => { 
+        section.style.display = 'block';
+        });
     } else {
       panel.classList.remove('active');
-      h3.classList.remove('rotateActive');
-      section.style.display = 'none';
+      h3.forEach(h3 => { 
+        h3.classList.remove('rotateActive');
+      });
+      section.forEach(section => {
+        section.style.display = 'none';
+         });
 
     }
   });
@@ -26,3 +34,11 @@ const panel_1 = document.querySelector('.panel_1');
 panel_1.addEventListener('click', e => {
   activateClass(e.target);
 });
+// const panel_2 = document.querySelector('.panel_2');
+// panel_1.addEventListener('click', e => {
+//   activateClass(e.target);
+// });
+// const panel_3 = document.querySelector('.panel_3');
+// panel_1.addEventListener('click', e => {
+//   activateClass(e.target);
+// });
