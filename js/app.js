96fcaa43bcd255ba396e6f2c033584dc45756618
Silvia <sramos.exp@gmail.com>
2020-09-29 10:27:46 +0100

    .rotate added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 //Toggle used to expand sections on click
 const panels = document.querySelectorAll('.panel');
 const div = document.querySelectorAll('.div');
+const h3 = document.querySelectorAll('h3');
 
-function toggleOpen() {
-  this.classList.toggle('open');
-  // div.style.display= 'block';
-}
+// function toggleOpen() {
+//   this.classList.toggle('open');
+  
+// }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 
 //-------------------------------------
 //Create event listener to hide/show info inside the panels on click
 // panels.addEventListener('click', () => {
 //   div.style.display = 'block';
+//   h3.classList.add('rotate');
 // });
